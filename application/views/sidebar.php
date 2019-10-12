46eaabb335cc705ea2de4791e1e514c2daa31c29
haseebvirk94 <haseebvirk94@gmail.com>
2019-10-12 20:49:50 +0500

    Final

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -107,6 +107,23 @@
         </div>
     </li>
 
+    <!-- Subject-->
+
+    <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseSubject" aria-expanded="true" aria-controls="collapsePages">
+            <i class="fas fa-fw fa-folder"></i>
+            <span>Subject</span>
+        </a>
+        <div id="collapseSubject" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Subject</h6>
+                <a class="collapse-item" href="<?php echo base_url() . 'Subject/addSubjectScreen' ?>">Add Subject</a>
+                <a class="collapse-item" href="<?php echo base_url() . 'subject' ?>">View Subject</a>
+                <div class="collapse-divider"></div>
+            </div>
+        </div>
+    </li>
+
 
     <!-- Nav Item - Charts -->
     <li class="nav-item">
