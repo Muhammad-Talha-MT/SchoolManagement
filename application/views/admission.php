314a2ad1c832ea45a9816700e62658ad3002a891
Mateen Ahmad <mateenahmad49957@gmail.com>
2019-10-16 23:46:19 +0500

    Picture Show on Choose file

--- application/views/admission.php
+++ application/views/admission.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<script type="text/javascript">
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#blah')
+                    .attr('src', e.target.result);
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
 <?php require('head.php'); ?>
 
 <body id="page-top">
@@ -25,17 +36,86 @@
                     <h1 class="h3 mb-4 text-gray-800">Student Admission</h1>
                     <div class="border-left-primary">
                         <div class="container">
-                            <form method="POST" enctype="multipart/form-data" action="<?php echo base_url() . 'Student/addStudent' ?>" class="needs-validation">
+                            <form method="POST" enctype="multipart/form-data"
+                                action="<?php echo base_url() . 'Student/addStudent' ?>" class="needs-validation">
                                 <div class="form-row">
-                                    <div class="form-group col-md-6">
+
+                                    <div class="form-group col-md-8">
+                                        <div class="form-row">
+                                            <div class="form-group col-md-6">
+                                                <label for="studentName">Student Name</label>
+                                                <input type="text" class="form-control" id="studentName"
+                                                    placeholder="Student Name" name="studentName">
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                                <label for="fatherName">Father Name</label>
+                                                <input type="text" class="form-control" id="fatherName"
+                                                    placeholder="Father Name" name="fatherName">
+                                            </div>
+                                        </div>
+                                        <div class="form-row">
+                                            <div class="form-group col-md-9">
+                                                <label for="inputAddress">Address</label>
+                                                <input type="text" class="form-control" id="address"
+                                                    placeholder="1234 Main St" name="address">
+                                            </div>
+                                            <div class="form-group col-md-3">
+                                                <label for="inputCity">Class</label>
+                                                <select class="form-control" name="class" required>
+                                                    <option>----Class----</option>
+                                                    <?php foreach ($classList as $class) { ?>
+                                                    <option value="<?php echo $class['id']; ?>">
+                                                        <?php echo $class['className']; ?></option>
+                                                    <?php } ?>
+                                                </select>
+                                            </div>
+
+                                        </div>
+                                        <div class="form-row">
+                                            <div class="form-group col-md-6">
+                                                <label for="studentCnic">Student CNIC / B-Form</label>
+                                                <input class="form-control" type="text" name="studentcnic"
+                                                    minlength="13" maxlength="13" id="studentcnic"
+                                                    placeholder="CNIC without dash " required>
+                                            </div>
+                                            <div class="form-group col-md-6">
+                                                <label for="fatherCnic">Faher CNIC</label>
+                                                <input class="form-control" type="text" name="fathercnic" minlength="13"
+                                                    maxlength="13" id="fathercnic" placeholder="CNIC without dash "
+                                                    required>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-md-4">
+                                        <img style="max-width: 180px; border-radius: 5px; margin: 35px 0px 10px 70px;"
+                                            id="blah" src="<?php echo base_url() . 'upload/download.png'; ?>"
+                                            alt="your image" />
+                                        <input
+                                            style="max-width: 180px; border-radius: 5px; max-height: 50px; margin-left: 70px; padding-left: 35px"
+                                            id="uploadPicture" type="file" name="uploadPicture"
+                                            onchange="readURL(this);" style="margin-left: 50px" required>
+                                    </div>
+
+
+
+
+                                </div>
+                                <!-- <div class="form-row">
+                                    <div class="form-group col-md-4">
                                         <label for="studentName">Student Name</label>
                                         <input type="text" class="form-control" id="studentName" placeholder="Student Name" name="studentName">
                                     </div>
-                                    <div class="form-group col-md-6">
+                                    <div class="form-group col-md-4">
                                         <label for="fatherName">Father Name</label>
                                         <input type="text" class="form-control" id="fatherName" placeholder="Father Name" name="fatherName">
                                     </div>
-                                </div>
+                                    <div class="form-group col-md-4">
+                                        <img style="max-width: 180px; border-radius: 5px; margin: 50px 0px 10px 70px;" id="blah" src="<?php echo base_url() . 'upload/blank-profile.png'; ?>"
+                                        alt="your image" />
+                                        <input style="max-width: 180px; border-radius: 5px; max-height: 50px; margin-left: 70px; padding-left: 35px"
+                                        id="uploadPicture" type="file" name="uploadPicture" onchange="readURL(this);" style="margin-left: 50px" required>
+                                    </div>
+                                </div> 
                                 <div class="form-row">
                                     <div class="form-group col-md-10">
                                         <label for="inputAddress">Address</label>
@@ -51,7 +131,7 @@
 
                                         </select>
                                     </div>
-                                </div>
+                                </div> 
                                 <div class="form-row">
                                     <div class="form-group col-md-6">
                                         <label for="studentCnic">Student CNIC / B-Form</label>
@@ -61,15 +141,17 @@
                                         <label for="fatherCnic">Faher CNIC</label>
                                         <input class="form-control" type="text" name="fathercnic" minlength="13" maxlength="13" id="fathercnic" placeholder="CNIC without dash " required>
                                     </div>
-                                </div>
+                                </div> -->
                                 <div class="form-row">
                                     <div class="form-group col-md-4">
                                         <label for="cellNumber">Cell Number</label>
-                                        <input class="form-control" type="text" name="cellNumber" minlength="11" maxlength="11" id="cellNumber" required>
+                                        <input class="form-control" type="text" name="cellNumber" minlength="11"
+                                            maxlength="11" id="cellNumber" required>
                                     </div>
                                     <div class="form-group col-md-4">
                                         <label for="dob">Date of Birth</label>
-                                        <input class="form-control" type="date" name="dob" required data-date="" data-date-format="DD MM YYYY">
+                                        <input class="form-control" type="date" name="dob" required data-date=""
+                                            data-date-format="DD MM YYYY">
                                     </div>
                                     <div class="form-group col-md-4">
                                         <label for="inputCity">Gender</label>
@@ -80,13 +162,18 @@
                                         </select>
                                     </div>
                                 </div>
-                                <div class="form-row">
+
+                                <!-- <div class="form-row">
                                     <div class="form-group col-md-3">
-                                        <input class="btn btn-success" id="uploadPicture" type="file" name="uploadPicture" required>
+                                        <input class="btn btn-success" required>
                                     </div>
-                                </div>
+                                </div> -->
                                 <button type="submit" class="btn btn-primary">Done</button>
                             </form>
+
+
+
+
                         </div>
                     </div>
 
