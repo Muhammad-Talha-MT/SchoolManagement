c4a30fa8e3340e8fbf25ff90a251ef8c2ebe970a
muhammadtalha112 <47237123+muhammadtalha112@users.noreply.github.com>
2019-11-27 21:56:31 +0500

    check
    dd

--- application/views/addClass.php
+++ application/views/addClass.php
@@ -1,8 +1,3 @@
-<?php
-if (!isset($_SESSION['id'])) {
-    redirect(base_url() . 'Login/showLogin');
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,69 +5,82 @@ if (!isset($_SESSION['id'])) {
 
 <body id="page-top">
 
-    <!-- Page Wrapper -->
-    <div id="wrapper">
+	<!-- Page Wrapper -->
+	<div id="wrapper">
 
-        <?php require('sidebar.php'); ?>
+		<?php require('sidebar.php'); ?>
 
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
+		<!-- Content Wrapper -->
+		<div id="content-wrapper" class="d-flex flex-column">
 
-            <!-- Main Content -->
-            <div id="content">
+			<!-- Main Content -->
+			<div id="content">
 
-                <?php require('topbar.php'); ?>
+				<?php require('topbar.php'); ?>
 
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
+				<!-- Begin Page Content -->
+				<div class="container-fluid">
 
-                    <?php
-                    if (isset($_SESSION['Success'])) {
-                        echo "<span class='alert alert-danger'>" . $_SESSION['Success'] . "</span><br><br>";
-                    }
-                    ?>
-                    <?php echo validation_errors('<div class="alert alert-danger">', '</div> '); ?>
-                    <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800">Add Class</h1>
-                    <div class="border-left-primary">
-                        <div class="container">
-                            <form method="POST" action="<?php echo base_url() . 'classes/addNewClass' ?>">
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="inputEmail4">Class Name</label>
-                                        <input class="form-control" id="className" placeholder="Enter Class Name" name="className" required>
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="inputPassword4">Monthly Fee</label>
-                                        <input class="form-control" id="fee" placeholder="Monthly Fee (Integer Value)" name="fee" pattern="\d+" required> </div>
-                                </div> <button type="submit" class="btn btn-primary">Save</button>
-                                <button type="reset" class="btn btn-secondary active" onclick="window.location.href = '<?php echo base_url() . '.Classes.' ?>';">Cancel</button>
-                            </form>
-                        </div>
-                    </div>
+					<!-- Page Heading -->
+					<h1 class="h3 mb-4 text-gray-800">Add Class</h1>
+					<div class="border-left-primary">
+						<div class="container">
+							<form method="POST" action="<?php echo base_url() . 'classes/addNewClass' ?>">
+								<div class="form-row">
+									<div class="form-group col-md-6">
+										<label for="inputEmail4">Class Name</label>
+										<input class="form-control" id="className" placeholder="Enter Class Name" name="className" required>
+									</div>
+									<div class="form-group col-md-6">
+										<label for="inputPassword4">Session Start</label>
+										<input type="date" class="form-control" id="className" placeholder="Enter Class Name" name="session" required>
+									</div>
+									<div class="form-group col-md-6">
+										<label for="inputPassword4">Monthly Fee</label>
+										<input class="form-control" id="fee" placeholder="Monthly Fee (Integer Value)" name="monthly" pattern="\d+" required>
+									</div>
+									<div class="form-group col-md-6">
+										<label for="inputPassword4">Admission Fee</label>
+										<input class="form-control" id="fee" placeholder="Admission Fee (Integer Value)" name="admission" pattern="\d+" required>
+									</div>
+									<div class="form-group col-md-6">
+										<label for="inputPassword4">Paper Fee</label>
+										<input class="form-control" id="fee" placeholder="Paper Fee (Integer Value)" name="paper" pattern="\d+" required>
+									</div>
+									<div class="form-group col-md-6">
+										<label for="inputPassword4">Course Fee</label>
+										<input class="form-control" id="fee" placeholder="Course Fee (Integer Value)" name="course" pattern="\d+" required>
+									</div>
 
-                </div>
-                <br />
-                <?php
-                if (isset($_SESSION['success'])) {
-                    echo  "<span class='alert alert-success'>" . $_SESSION['success'] . "</span>";
-                }
-                ?>
-                <!-- /.container-fluid -->
+								</div>
+								<button type="submit" class="btn btn-primary">Save</button>
+								<button type="reset" class="btn btn-secondary active" onclick="window.location.href = '<?php echo base_url() . '.Classes.' ?>';">Cancel</button>
+							</form>
+						</div>
+					</div>
 
-            </div>
-            <!-- End of Main Content -->
+				</div>
+				<br />
+				<?php
+				if (isset($_SESSION['success'])) {
+					echo  "<span class='alert alert-success'>" . $_SESSION['success'] . "</span>";
+				}
+				?>
+				<!-- /.container-fluid -->
 
-            <?php require('footbar.php'); ?>
+			</div>
+			<!-- End of Main Content -->
 
-        </div>
-        <!-- End of Content Wrapper -->
+			<?php require('footbar.php'); ?>
 
-    </div>
-    <!-- End of Page Wrapper -->
+		</div>
+		<!-- End of Content Wrapper -->
+
+	</div>
+	<!-- End of Page Wrapper -->
 
 
-    <?php require('foot.php'); ?>
+	<?php require('foot.php'); ?>
 
 </body>
 
--- application/views/editClass.php
+++ application/views/editClass.php
@@ -5,65 +5,81 @@
 
 <body id="page-top">
 
-    <!-- Page Wrapper -->
-    <div id="wrapper">
+	<!-- Page Wrapper -->
+	<div id="wrapper">
 
-        <?php require('sidebar.php'); ?>
+		<?php require('sidebar.php'); ?>
 
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
+		<!-- Content Wrapper -->
+		<div id="content-wrapper" class="d-flex flex-column">
 
-            <!-- Main Content -->
-            <div id="content">
+			<!-- Main Content -->
+			<div id="content">
 
-                <?php require('topbar.php'); ?>
+				<?php require('topbar.php'); ?>
 
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
+				<!-- Begin Page Content -->
+				<div class="container-fluid">
 
-                    <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800">Edit Class</h1>
-                    <div class="border-left-primary">
-                        <div class="container">
-                            <form method="POST" action="<?php echo base_url() . 'Classes/showEdit/' . $class['id'] ?>">
-                                <div class="form-row">
-                                    <div class="form-group col-md-6">
-                                        <label for="inputEmail4">Class Name</label>
-                                        <input class="form-control" id="className" placeholder="Name" name="className" required value="<?php echo set_value('className', $class['className']); ?>">
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="inputPassword4">Monthly Fee</label>
-                                        <input class="form-control" id="fee" name="fee" placeholder="Monthly Fee (Integer Value)" pattern="\d+" required value="<?php echo set_value('fee', $class['fee']); ?>">
-                                    </div>
-                                </div> <button type="submit" class="btn btn-primary ">Done</button>
-                                <button type="reset" class="btn btn-secondary active" onclick="window.location.href = '<?php echo base_url() . '.Classes.' ?>';">Cancel</button>
-                            </form>
-                        </div>
-                    </div>
+					<!-- Page Heading -->
+					<h1 class="h3 mb-4 text-gray-800">Edit Class</h1>
+					<div class="border-left-primary">
+						<div class="container">
+							<form method="POST" action="<?php echo base_url() . 'Classes/showEdit/' . $class['id'] ?>">
+								<div class="form-row">
+									<div class="form-group col-md-6">
+										<label for="inputEmail4">Class Name</label>
+										<input class="form-control" id="className" placeholder="Name" name="className" required value="<?php echo set_value('className', $class['className']); ?>">
+									</div>
+									<div class="form-group col-md-6">
+										<label for="inputPassword4">Session Start</label>
+										<input type="date" class="form-control" id="className" placeholder="Enter Class Name" name="session" required>
+									</div>
+									<div class="form-group col-md-6">
+										<label for="inputPassword4">Monthly Fee</label>
+										<input class="form-control" id="fee" placeholder="Monthly Fee (Integer Value)" name="monthly" pattern="\d+" required>
+									</div>
+									<div class="form-group col-md-6">
+										<label for="inputPassword4">Admission Fee</label>
+										<input class="form-control" id="fee" placeholder="Admission Fee (Integer Value)" name="admission" pattern="\d+" required>
+									</div>
+									<div class="form-group col-md-6">
+										<label for="inputPassword4">Paper Fee</label>
+										<input class="form-control" id="fee" placeholder="Paper Fee (Integer Value)" name="paper" pattern="\d+" required>
+									</div>
+									<div class="form-group col-md-6">
+										<label for="inputPassword4">Course Fee</label>
+										<input class="form-control" id="fee" placeholder="Course Fee (Integer Value)" name="course" pattern="\d+" required>
+									</div>
+								</div> <button type="submit" class="btn btn-primary ">Done</button>
+								<button type="reset" class="btn btn-secondary active" onclick="window.location.href = '<?php echo base_url() . '.Classes.' ?>';">Cancel</button>
+							</form>
+						</div>
+					</div>
 
-                </div>
-                <br />
+				</div>
+				<br />
 
-                <?php
-                if (isset($_SESSION['success'])) {
-                    echo  "<span class='alert alert-success'>" . $_SESSION['success'] . "</span>";
-                }
-                ?>
-                <!-- /.container-fluid -->
+				<?php
+				if (isset($_SESSION['success'])) {
+					echo  "<span class='alert alert-success'>" . $_SESSION['success'] . "</span>";
+				}
+				?>
+				<!-- /.container-fluid -->
 
-            </div>
-            <!-- End of Main Content -->
+			</div>
+			<!-- End of Main Content -->
 
-            <?php require('footbar.php'); ?>
+			<?php require('footbar.php'); ?>
 
-        </div>
-        <!-- End of Content Wrapper -->
+		</div>
+		<!-- End of Content Wrapper -->
 
-    </div>
-    <!-- End of Page Wrapper -->
+	</div>
+	<!-- End of Page Wrapper -->
 
 
-    <?php require('foot.php'); ?>
+	<?php require('foot.php'); ?>
 
 </body>
 
